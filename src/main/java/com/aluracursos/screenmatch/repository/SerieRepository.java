5a1a1d9fa5c085d02acba0b1b8c2c67b957ffd9c
diana <dianaclabel08@gmail.com>
2025-01-29 22:51:16 -0500

    se agrega funcionalidad para buscar serie por evaluacion y temporadas con JPQL

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -4,6 +4,7 @@ import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,8 @@ public interface SerieRepository extends JpaRepository <Serie,Long> {
     List<Serie> findTop5ByOrderByEvaluacionDesc();
     List <Serie> findByGenero(Categoria categoria);
     List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(Integer totalTemporadas, double evaluacion);
-
+    //@Query( value ="SELECT * FROM series WHERE series.total_temporadas <= 6 AND series.evaluacion >=7.5, nativeQuery = true", nativeQuery = true)
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.evaluacion >= :evaluacion ")
+    List<Serie> seriesPorTemporadaYEvaluacion(int totalTemporadas, Double evaluacion);
 
 }
